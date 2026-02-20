4345ff959ba800b8576d2278dd1ba80e8da719b4
Ayush Karnewar <ayushkarnewar369@gmail.com>
2026-02-20 12:36:34 +0530

    Add decrypt animation effect to text

--- components/DecryptText/index.js
+++ components/DecryptText/index.js
@@ -2,19 +2,51 @@ import { useEffect, useState } from 'react'
 
 const DecryptText = ({
     values = ['Empty'],
-    delay = 5000
+    delay = 3000
 }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [displayText, setDisplayText] = useState('');
+  const [isDecrypting, setIsDecrypting] = useState(true);
+
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*';
 
   useEffect(() => {
-    const action = setInterval(() => {
+    const targetText = values[currentIndex];
+    let iteration = 0;
+    
+    const decryptInterval = setInterval(() => {
+      setDisplayText(
+        targetText
+          .split('')
+          .map((char, index) => {
+            if (index < iteration) {
+              return targetText[index];
+            }
+            return characters[Math.floor(Math.random() * characters.length)];
+          })
+          .join('')
+      );
+
+      if (iteration >= targetText.length) {
+        clearInterval(decryptInterval);
+        setIsDecrypting(false);
+      }
+
+      iteration += 1 / 3;
+    }, 30);
+
+    const changeTextTimeout = setTimeout(() => {
+      setIsDecrypting(true);
       setCurrentIndex((prev) => (prev + 1) % values.length);
     }, delay);
 
-    return () => clearInterval(action);
-  }, [values, delay]);
+    return () => {
+      clearInterval(decryptInterval);
+      clearTimeout(changeTextTimeout);
+    };
+  }, [currentIndex, values, delay]);
 
-  return <>{values[currentIndex]}</>;
+  return <>{displayText || values[0]}</>;
 };
 
 DecryptText.propTypes = {}
